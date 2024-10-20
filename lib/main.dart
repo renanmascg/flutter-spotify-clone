ab56ba359b882193746e17f62426a41a1e9cb406
Renan Mascarenhas <renanmascg@gmail.com>
2024-10-19 23:50:20 -0300

    feat: Adiciona splash screen na aplicação.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/core/configs/theme/app_theme.dart';
+import 'package:spotify_clone/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,13 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
-      home: Container(),
+      debugShowCheckedModeBanner: false,
+      home: const SplashPage(),
     );
   }
 }
